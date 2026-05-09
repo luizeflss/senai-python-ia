bcb012406c5c6995f5088ae5d6cdb9bfdb2b031e
luizeflss <luizeduardu234@gmail.com>
2026-05-09 17:32:25 -0300

    aula 01 introdução python data: 09/05/2026

--- AULA01/dicionario.py
+++ AULA01/dicionario.py
@@ -1,1 +1,12 @@
-perfilUsuario = {}
+perfilUsuario = {
+    "Nome":"Luiz",
+    "Idade":"19 anos",
+    "Cidade":"Ibiuna",
+    "Interesses":["Games", "Musicas", "Filmes"]
+}
+
+for chave in perfilUsuario:
+    print(f"Chave: {chave}")
+
+for valor in perfilUsuario.values():
+    print(f"Valor: {valor}")
